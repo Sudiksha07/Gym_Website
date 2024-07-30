1388269a05e5a20ca1cc25d1973141507b884df2
Sudiksha Gandhi <sudiksha.gandhi07@gmailcom>
2024-07-30 23:00:00 +0530

    first

--- src/Components/SearchExercies.js
+++ src/Components/SearchExercies.js
@@ -4,7 +4,7 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/FetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+const SearchExercises = ({  bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('');
   const [bodyParts, setBodyParts] = useState([]);
 
@@ -18,23 +18,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     fetchExercisesData();
   }, []);
 
-  const handleSearch = async () => {
-    if (search) {
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-      console.log("search data is: ",search);
-      const searchedExercises = exercisesData.filter(
-        (item) => item.name.toLowerCase().includes(search)
-               || item.target.toLowerCase().includes(search)
-               || item.equipment.toLowerCase().includes(search)
-               || item.bodyPart.toLowerCase().includes(search),
-      );
 
-      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-
-      setSearch('');
-      setExercises(searchedExercises);
-    }
-  };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ export default function Home() {
         setExercises={setExercises}
         bodyPart={bodyPart}
         setBodyPart={setBodyPart}
+       
       />
       <Exercises
         setExercises={setExercises}
